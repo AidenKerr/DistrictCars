e33f6aff3633a14568457b0a48a6a62b7d0f5a5b
AidenKerr <arkerr2000@gmail.com>
2018-01-25 23:58:20 -0800

    Initial commit

---
+++ Assets/Scripts/Driving.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driving : MonoBehaviour {
+
+	public WheelCollider wc;
+	public bool steerable;
+	public float maxTorque;
+	public float maxTurn;
+
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+
+		wc.motorTorque = Input.GetAxis("Vertical") * maxTorque;
+	
+		if (steerable) {
+			wc.steerAngle = Input.GetAxis("Horizontal") * maxTurn;
+		}
+
+	}
+}
