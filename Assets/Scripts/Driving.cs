8a3abb642a5e23a5afcf35e26cb389ada527f7c4
AidenKerr <arkerr2000@gmail.com>
2018-01-29 22:46:35 -0800

    add camera smoothing, change car setup

--- Assets/Scripts/Driving.cs
+++ Assets/Scripts/Driving.cs
@@ -4,22 +4,54 @@ using UnityEngine;
 
 public class Driving : MonoBehaviour {
 
-	public WheelCollider wc;
-	public bool steerable;
+	public List<AxleInfo> axleInfos;
 	public float maxTorque;
 	public float maxTurn;
 
-	void Start () {
-		
+	public void ApplyVisuals(WheelCollider collider) {
+		if (collider.transform.childCount == 0) {
+			return;
+		}
+
+		Transform visualWheel = collider.transform.GetChild(0);
+
+		Vector3 position;
+		Quaternion rotation;
+		collider.GetWorldPose(out position, out rotation);
+	
+		visualWheel.transform.position = position;
+		visualWheel.transform.rotation = rotation;
+
 	}
 
-	void FixedUpdate () {
 
-		wc.motorTorque = Input.GetAxis("Vertical") * maxTorque;
-	
-		if (steerable) {
-			wc.steerAngle = Input.GetAxis("Horizontal") * maxTurn;
+	void FixedUpdate() {
+
+		// set motor and steering values
+		float motor = Input.GetAxis("Vertical") * maxTorque;
+		float steering = Input.GetAxis("Horizontal") * maxTurn;
+
+		// apply motor and steering values when needed
+		foreach (AxleInfo axleInfo in axleInfos) {
+			if (axleInfo.motor) {
+				axleInfo.leftWheel.motorTorque = motor;
+				axleInfo.rightWheel.motorTorque = motor;
+			}
+			if (axleInfo.steerable) {
+				axleInfo.leftWheel.steerAngle = steering;
+				axleInfo.rightWheel.steerAngle = steering;
+			}
+			ApplyVisuals(axleInfo.leftWheel);
+			ApplyVisuals(axleInfo.rightWheel);
 		}
 
 	}
+}
+
+[System.Serializable]
+public class AxleInfo {
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public bool motor; // are these wheels connected to motor?
+	public bool steerable; // are these wheels steerable?
 }
