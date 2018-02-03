bd03198a70cb7a31870f625055b3509cfbebd95e
AidenKerr <arkerr2000@gmail.com>
2018-02-03 00:49:33 -0800

    Add variable turning angle

--- Assets/Scripts/Driving.cs
+++ Assets/Scripts/Driving.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Driving : MonoBehaviour {
 
+	public Rigidbody rb; // rigidbody for velocity purposes
 	public List<AxleInfo> axleInfos;
 	public float maxTorque;
 	public float maxTurn;
+	public float maxWheelTurnVelocity; // velocity at which wheel turning becomes minimal;
+
+	private float velocityBase;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		velocityBase = Mathf.Pow(10, Mathf.Log10(maxTurn) / maxWheelTurnVelocity);
+	}
 
 	public void ApplyVisuals(WheelCollider collider) {
 		if (collider.transform.childCount == 0) {
@@ -24,15 +33,21 @@ public class Driving : MonoBehaviour {
 
 	}
 
-
 	void FixedUpdate() {
+
+		float speed = rb.velocity.magnitude;
+		print("velocity = " + speed);
 
 		// set motor and steering values
 		float motor = Input.GetAxis("Vertical") * maxTorque;
-		float steering = Input.GetAxis("Horizontal") * maxTurn;
+		// Apply the exponentially decreasing turning angle
+		// The ternary operator is to limit it at 90% of it's decrease so it never reaches 0 turning angle
+		float a = (speed > 0.9f * maxWheelTurnVelocity) ? maxTurn - Mathf.Pow(velocityBase, 0.9f * maxWheelTurnVelocity) : maxTurn - Mathf.Pow(velocityBase, speed);
+		float steering = Input.GetAxis("Horizontal") * a;
 
 		// apply motor and steering values when needed
 		foreach (AxleInfo axleInfo in axleInfos) {
+
 			if (axleInfo.motor) {
 				axleInfo.leftWheel.motorTorque = motor;
 				axleInfo.rightWheel.motorTorque = motor;
@@ -52,6 +67,6 @@ public class Driving : MonoBehaviour {
 public class AxleInfo {
 	public WheelCollider leftWheel;
 	public WheelCollider rightWheel;
-	public bool motor; // are these wheels connected to motor?
+	public bool motor; // are these wheels connected to the motor?
 	public bool steerable; // are these wheels steerable?
 }
