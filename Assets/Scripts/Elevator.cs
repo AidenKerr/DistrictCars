f267a4ca44a6ab4d6997256a3d0b20fba21ff48c
AidenKerr <arkerr2000@gmail.com>
2018-03-21 15:49:03 -0700

    Add New Levels

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+
+	public float freq;
+	public float amp;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.up * Mathf.Sin(Time.timeSinceLevelLoad * freq) * amp);
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public float rate;
+	public Vector3 axis;
+
+	void Update() {
+		transform.Rotate(axis * Time.deltaTime * rate);
+	}
+}
