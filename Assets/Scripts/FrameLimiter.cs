8e5f37da9182a0a90598f6dbf560ccbe19ebe69a
AidenKerr <arkerr2000@gmail.com>
2018-04-15 01:19:45 -0700

    Add Frame Limiter (debug)
    added a frame limiter for debug purposes.

---
+++ Assets/Scripts/FrameLimiter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameLimiter : MonoBehaviour {
+
+	public int fps;
+
+	void Awake () {
+		QualitySettings.vSyncCount = 0;  // VSync must be disabled
+		Application.targetFrameRate = fps;
+	}
+
+}
